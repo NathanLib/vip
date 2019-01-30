05228b124c0b41c65fbd7ab94d31571b7b4bc416
Nathan Liboutet <nathan.lib19@gmail.com>
2019-01-30 11:31:35 +0100

    add list first letter

--- controllers/VipController.js
+++ controllers/VipController.js
@@ -1,10 +1,18 @@
 
-
+let model = require("../models/vip.js");
 
 // ///////////////////////// R E P E R T O I R E    D E S     S T A R S
 
 module.exports.Repertoire = 	function(request, response){
    response.title = 'Répertoire des stars';
 
-      response.render('repertoireVips', response);
-  } ;
+    model.Repertoire(function(err, result){
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        response.firstLetter = result;
+        response.render('repertoireVips', response);
+    } );
+}
--- models/vip.js
+++ models/vip.js
@@ -11,3 +11,15 @@ module.exports.test = function(callback) {
         }
     });
 };
+
+module.exports.Repertoire = function(callback) {
+    db.getConnection(function(err, connexion) {
+        if (!err) {
+            let sql = "SELECT DISTINCT LEFT(VIP_NOM,1) AS letter FROM vip ORDER BY letter;";
+
+            console.log(sql);
+            connexion.query(sql, callback);
+            connexion.release();
+        }
+    });
+}
